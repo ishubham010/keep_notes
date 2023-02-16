eda15f5d2ddf7ca358c85c0952828fb696600aed
Shubham Choubey <ishubham010@gmail.com>
2023-02-16 20:03:41 +0530

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from './Images/logo.png';
+
+const Header = () => {
+    return (
+        <>
+        <div className="header">
+        <img src= {logo} alt='logo' width='60' height='50' />
+        <h1>Keep Notes</h1>
+        </div>
+        </>
+    );
+};
+
+export default Header;
